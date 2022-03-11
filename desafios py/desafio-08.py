3ff8c718d0348eea25e433bc32bdcce562aaf53c
Caio Felipe <75501510+caiovfelipe@users.noreply.github.com>
2022-03-10 22:11:56 -0300

    Update desafio-08.py

--- desafios py/desafio-08.py
+++ desafios py/desafio-08.py
@@ -1,4 +1,8 @@
 metros = float(input('Quantos metros? '))
+kilometro = metros / 1000
+hectometro = metros / 100
+decametro = metros / 10
+decimetro = metros * 10
 centimetros = metros*100
 milimetros = metros*1000
-print('Ele tem {} centimetros e {} milimetros.'.format(centimetros, milimetros))
+print('Ele tem {} kilometros, {} hectometros, {} decametros, {} decimetros, {} centimetros e {} milimetros.'.format(kilometro, hectometro, decametro, decimetro, centimetros, milimetros))
