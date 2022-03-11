b9691c240cafcae80f1a4a9267d9d8605cee5a1e
Caio Felipe <75501510+caiovfelipe@users.noreply.github.com>
2022-03-10 22:08:09 -0300

    Update desafio-06.py

--- desafios py/desafio-06.py
+++ desafios py/desafio-06.py
@@ -2,4 +2,4 @@ num = int(input('Escolha um numero: '))
 num_dobro = num*2
 num_triplo = num*3
 num_raiz = num**(1/2)
-print('Seu dobro é {}, seu triplo é {}, e sua raiz é {}. '.format(num_dobro, num_triplo, num_raiz))
+print('Seu dobro é {}, seu triplo é {}, e sua raiz é {:.2f}. '.format(num_dobro, num_triplo, num_raiz))
