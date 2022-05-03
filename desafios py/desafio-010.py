cbe1721a588b82edeaad6e5b985e6134b1dc8592
Caio Felipe <75501510+caiovfelipe@users.noreply.github.com>
2022-05-03 16:16:09 -0300

    atualizações-aula
    Atualização para melhorar uns detalhes no programa

--- desafios py/desafio-010.py
+++ desafios py/desafio-010.py
@@ -1,3 +1,3 @@
 reais = float(input('Qual o valor em reais que você quer transferir para dolar? R$'))
 dolar = reais / 5.12
-print('O valor de {} reais pode valer {} dolares'.format(reais, dolar))
+print('O valor de {} reais pode valer {:.2f} dolares'.format(reais, dolar))
