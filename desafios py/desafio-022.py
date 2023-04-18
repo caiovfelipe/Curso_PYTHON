d6c6520b14b19d690947dec7f72e47946820968d
Caio Felipe <75501510+caiovfelipe@users.noreply.github.com>
2023-04-18 15:32:05 -0300

    Desafio 022 e 023, e screenshots dos desafios

---
+++ desafios py/desafio-022.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome completo:')).strip()
+print('Seu nome em letras maiusculas é.. {}'.format(nome.upper()))
+print('Seu nome em letras minusculas é.. {}'.format(nome.lower()))
+print('Seu nome tem ao todo.. {}'.format(len(nome) - nome.count(' ')))
+separado = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras.'.format(separado[0], len(separado[0])))
---
+++ desafios py/desafio-023.py
@@ -0,0 +1,7 @@
+number = int(input('Digite um numero entre 0 e 9999: '))
+n = str(number)
+print('Frist number is {}'.format(n[0]))
+print('Frist number is {}'.format(n[1]))
+print('Frist number is {}'.format(n[2]))
+print('Frist number is {}'.format(n[3]))
+
