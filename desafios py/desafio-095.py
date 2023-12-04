cf4493e8df46b3c6eb96ea50ed6c10e2b2e1892d
Caio Felipe <caiofelipe32876@gmail.com>
2023-12-04 11:01:58 -0300

    Erro do ex95 resolvido
    O erro consistia em não possuir uma linha para redefinir a 'listajogos' a um valor vazio. Isso fazia com que a lista de jogos fosse a mesma para todos os jogadores.

--- desafios py/desafio-095.py
+++ desafios py/desafio-095.py
@@ -1,23 +1,25 @@
 lista = list()
-listajogos = list()
+
 dicionario = dict()
 
 quant_jogadores = int(input('Quantos jogadores serão contabilizados?: '))
 
 for z in range(quant_jogadores):
     dicionario["Nome"] = str(input(f'Nome do jogador {z+1}: '))
+    listajogos = list()
     quant_jogos = int(input('Quantos jogos foram jogados?: '))
     for c in range(quant_jogos):
         golsc = int(input(f'Número de gols no jogo {c+1}: '))
         listajogos.append({f'Jogo': c+1, 'Gols': golsc})
-    lista.append({'Jogador': dicionario, 'Jogos': listajogos})
+    lista.append({'Jogador': dicionario.copy(), 'Jogos': listajogos})
+    
 
-for jogador in lista:
-    nome = jogador['Jogador']["Nome"]
+for jogadorc in lista:
+    nome = jogadorc['Jogador']["Nome"]
     print(f'Dados do jogador {nome}')
-    for jogo in jogador['Jogos']:
-        num_jogo = jogo['Jogo']
-        num_gols = jogo['Gols']
+    for jogoc in jogadorc['Jogos']:
+        num_jogo = jogoc['Jogo']
+        num_gols = jogoc['Gols']
         print(f'No jogo {num_jogo}, a quantidade de gols foi: {num_gols}')
 
 #Nota
