4e96b4d84f226e66f66081396fc09e7a726eee27
Caio Felipe <caiofelipe32876@gmail.com>
2024-01-13 20:08:29 -0300

    bug fixed in ex098

--- desafios py/desafio-098.py
+++ desafios py/desafio-098.py
@@ -1,5 +1,15 @@
 def contador(inicio, fim, passo):
+    if passo == 0:
+            passo = 1
+    if inicio > fim:
+            passo *= -1
+            fim -= 1
+    else:
+            fim += 1
+    if (inicio > fim):
+          fim -= 1
     for c in range(inicio-1, fim, passo):
+        
         print(f"{c+1} ", end="")
 
 
